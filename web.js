2d6458ca766bffe5f08267d77e5251be0a0d5caf
Ubuntu <ubuntu@ip-10-164-51-146.ec2.internal>
2013-07-06 03:21:23 +0000

    updated files to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var content = fs.readFileSync('./index.html','utf8');
+    response.send(content);
 });
 
 var port = process.env.PORT || 5000;
